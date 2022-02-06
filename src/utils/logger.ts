fd4d4d10aa9348ecb108d3fa076a6085735ae3dc
Brian <brianpagal@gmail.com>
2022-02-06 19:01:22 +0800

    refactor: 💡 logger funcs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,13 +1,20 @@
 import chalk from 'chalk';
 
+export let counter = 1;
+
+const getMsg = (msg: string) => `${counter}. ${msg.toUpperCase()}\n`;
+
 export const logProcess = (msg: string) => {
-  console.log(chalk.bgBlue(msg.toUpperCase()));
+  console.log(chalk.bgBlue(getMsg(msg)));
+  counter++;
 };
 
 export const logSuccess = (msg: string) => {
-  console.log(chalk.bgGreen(msg.toUpperCase()));
+  console.log(chalk.bgGreen(getMsg(msg)));
+  counter++;
 };
 
 export const logError = (msg: string) => {
-  console.log(chalk.bgRed(msg.toUpperCase()));
+  console.log(chalk.bgRed(getMsg(msg)));
+  counter++;
 };
