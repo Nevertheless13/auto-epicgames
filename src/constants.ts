d1498b46328af0c9fd25ac437a9109bc99114479
Brian <brianpagal@gmail.com>
2022-02-08 10:33:43 +0800

    feat: 🎸 constants and improve logger messages

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const WAITFOR_OPTIONS = {
+  visible: true,
+};
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,16 +5,16 @@ export let counter = 1;
 const getMsg = (msg: string) => `${counter}. ${msg.toUpperCase()}\n`;
 
 export const logProcess = (msg: string) => {
-  console.log(chalk.bgBlue(getMsg(msg)));
+  console.log(chalk.black.bgBlue(getMsg(msg)));
   counter++;
 };
 
 export const logSuccess = (msg: string) => {
-  console.log(chalk.bgGreen(getMsg(msg)));
+  console.log(chalk.black.bgGreen(getMsg(msg)));
   counter++;
 };
 
 export const logError = (msg: string) => {
-  console.log(chalk.bgRed(getMsg(msg)));
+  console.log(chalk.black.bgRed(getMsg(msg)));
   counter++;
 };
